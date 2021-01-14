81d211bc925f2ac7298e39040f5d85a3dd12d6e7
sergey2324 <sergm2324@gmail.com>
2021-01-14 18:08:02 +0300

    сделал загрузку комментариев

--- src/App.vue
+++ src/App.vue
@@ -32,35 +32,33 @@
     </div>
   </div>
   <div class="container">
-    <p>
-      <button class="btn primary">Загрузить комментарии</button>
+    <p v-if="!loading && comments.length === 0">
+      <button class="btn primary" @click="fetchComments">Загрузить комментарии</button>
     </p>
-    <div class="card">
-      <h2>Комментарии</h2>
-      <ul class="list">
-        <li class="list-item">
-          <div>
-            <p><strong>test@microsoft.com</strong></p>
-            <small>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi, reiciendis.</small>
-          </div>
-        </li>
-      </ul>
-    </div>
     <div class="loader" v-if="loading"></div>
+    <app-comments :comments = 'comments'></app-comments>
   </div>
 </template>
 
 <script>
+import AppComments from '@/AppComments'
 import axios from 'axios'
 export default {
   data() {
     return {
-      loading: false
+      loading: false,
+      comments: []
     }
   },
   methods: {
-    //
-  }
+    async fetchComments() {
+      this.loading = true
+      const {data} = await axios.get('https://jsonplaceholder.typicode.com/comments?_limit=42')
+      this.comments = data
+      this.loading = false
+    }
+  },
+  components: {AppComments}
 }
 </script>
 
---
+++ src/AppComments.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card" v-if = "comments.length !== 0">
+    <h2>Комментарии</h2>
+    <ul class="list">
+      <li class="list-item" v-for = "comment in comments" :key="comments.id">
+        <div>
+          <p><strong>{{comment.email}}</strong></p>
+          <small>{{comment.body}}</small>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppComments",
+  props: ['comments']
+}
+</script>
+
+<style scoped>
+
+</style>
